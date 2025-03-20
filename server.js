93db8c7b04d1dbab1f302a4d478dc0c8ec43793a
Maxx2774 <maxviggonordin@hotmail.se>
2025-03-20 13:39:37 +0100

    .

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(productRoutes);
 app.use(authRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
