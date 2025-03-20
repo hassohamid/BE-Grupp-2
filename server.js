5199d745e94cbb22bcd6e608906530fccadd1b3a
Maxx2774 <maxviggonordin@hotmail.se>
2025-03-20 12:34:24 +0100

    cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
 
 const productRoutes = require("./src/products/routes/ProductRoutes");
